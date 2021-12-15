8c26d6b20412718419d8491875a535ca80ea28bf
xudehui <15707039958@qq.com>
2021-12-15 21:16:50 +0800

    完成添加菜单功能

--- src/views/modules/product/category.vue
+++ src/views/modules/product/category.vue
@@ -1,37 +1,51 @@
 <template>
-  <el-tree
-    :data="menus"
-    :props="defaultProps"
-    @node-click="handleNodeClick"
-    :expand-on-click-node="false"
-    show-checkbox
-    node-key="catId"
-    :default-expanded-keys="expandedKey"
-  >
-    <span class="custom-tree-node" slot-scope="{ node, data }">
-      <span>{{ node.label }}</span>
-      <span>
-        <!-- 一级分类和二级分类才可以添加子分类 -->
-        <el-button
-          v-if="node.level <= 2"
-          type="text"
-          size="mini"
-          @click="() => append(data)"
-        >
-          添加分类
-        </el-button>
-        <!-- 只有没有子分类的情况才可以删除该分类 -->
-        <el-button
-          v-if="node.childNodes.length == 0"
-          type="text"
-          size="mini"
-          @click="() => remove(node, data)"
-        >
-          删除分类
-        </el-button>
+  <div>
+    <el-tree
+      :data="menus"
+      :props="defaultProps"
+      @node-click="handleNodeClick"
+      :expand-on-click-node="false"
+      show-checkbox
+      node-key="catId"
+      :default-expanded-keys="expandedKey"
+    >
+      <span class="custom-tree-node" slot-scope="{ node, data }">
+        <span>{{ node.label }}</span>
+        <span>
+          <!-- 一级分类和二级分类才可以添加子分类 -->
+          <el-button
+            v-if="node.level <= 2"
+            type="text"
+            size="mini"
+            @click="() => append(data)"
+          >
+            添加分类
+          </el-button>
+          <!-- 只有没有子分类的情况才可以删除该分类 -->
+          <el-button
+            v-if="node.childNodes.length == 0"
+            type="text"
+            size="mini"
+            @click="() => remove(node, data)"
+          >
+            删除分类
+          </el-button>
+        </span>
       </span>
-    </span>
-  </el-tree>
+    </el-tree>
+
+    <el-dialog title="添加分类" :visible.sync="dialogFormVisible">
+      <el-form :model="category">
+        <el-form-item label="分类名称">
+          <el-input v-model="category.name" autocomplete="off"></el-input>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="dialogFormVisible = false">取 消</el-button>
+        <el-button type="primary" @click="addCategory()">确 定</el-button>
+      </div>
+    </el-dialog>
+  </div>
 </template>
 
 <script>
@@ -50,6 +64,15 @@ export default {
         label: "name",
       },
       expandedKey: [],
+      category: {
+        name: "",
+        parentCid: 0,
+        catLevel: 0,
+        showStatus: 0,
+        sort: 0,
+        productCount: 0,
+      },
+      dialogFormVisible: false,
     };
   },
   // 计算属性 类似于data概念
@@ -72,40 +95,80 @@ export default {
       });
     },
 
+    // 添加菜单
     append(data) {
+      // 1. 显示表单
+      this.dialogFormVisible = true;
 
+      // 2. 封装数据
+      this.category.parentCid = data.catId;
+      this.category.catLevel = data.catLevel * 1 + 1;
+      this.category.showStatus = 1;
+      this.category.sort = 0;
+      this.category.productCount = 0;
     },
 
+    // 添加菜单
+    addCategory() {
+      this.$http({
+        url: this.$http.adornUrl("/product/category/save"),
+        method: "post",
+        data: this.$http.adornData(this.category, false),
+      }).then(({ data }) => {
+        if (data && data.code === 0) {
+          this.$message({
+            message: "操作成功",
+            type: "success",
+            duration: 1500,
+            onClose: () => {
+              // 1. 关闭表单
+              this.dialogFormVisible = false;
+              // 2. 获取新的分类数据
+              this.getTreeMenuData();
+              // 3. 默认展开之前删除分类的父分类
+              this.expandedKey = [this.category.parentCid];
+              // 4. 清空 category 对象
+              this.category = {};
+            },
+          });
+        } else {
+          this.$message.error(data.msg);
+        }
+      });
+    },
+
+    // 删除菜单
     remove(node, data) {
-      console.log(node, data)
-        const ids = [node.data.catId]
-        this.$confirm(`确定删除【${node.data.name}】菜单项?`, '提示', {
-          confirmButtonText: '确定',
-          cancelButtonText: '取消',
-          type: 'warning'
-        }).then(() => {
+      const ids = [node.data.catId];
+      this.$confirm(`确定删除【${node.data.name}】菜单项?`, "提示", {
+        confirmButtonText: "确定",
+        cancelButtonText: "取消",
+        type: "warning",
+      })
+        .then(() => {
           this.$http({
-            url: this.$http.adornUrl('/product/category/delete'),
-            method: 'post',
-            data: this.$http.adornData(ids, false)
-          }).then(({data}) => {
+            url: this.$http.adornUrl("/product/category/delete"),
+            method: "post",
+            data: this.$http.adornData(ids, false),
+          }).then(({ data }) => {
             if (data && data.code === 0) {
               this.$message({
-                message: '操作成功',
-                type: 'success',
+                message: "操作成功",
+                type: "success",
                 duration: 1500,
                 onClose: () => {
                   // 1. 获取新的分类数据
-                  this.getTreeMenuData()
+                  this.getTreeMenuData();
                   // 2. 默认展开之前删除分类的父分类
-                  this.expandedKey = [node.data.parentCid]
-                }
-              })
+                  this.expandedKey = [node.data.parentCid];
+                },
+              });
             } else {
-              this.$message.error(data.msg)
+              this.$message.error(data.msg);
             }
-          })
-        }).catch(() => {})
+          });
+        })
+        .catch(() => {});
     },
   },
   // 生命周期 - 创建完成（可以访问当前this实例）
