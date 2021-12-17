76692f4904bda7b2f1c08b0d0d479183b667378c
xudehui <15707039958@qq.com>
2021-12-17 18:28:23 +0800

    完成拖拽功能、批量拖拽功能、批量删除功能

--- src/views/modules/product/category.vue
+++ src/views/modules/product/category.vue
@@ -1,5 +1,15 @@
 <template>
   <div>
+    <el-switch
+      v-model="draggable"
+      active-text="开启拖拽"
+      inactive-text="关闭拖拽"
+      style="padding: 0px 30px 0 10px"
+    >
+    </el-switch>
+    <el-button type="primary" @click="batchDrag()">批量拖拽</el-button>
+    <el-button type="danger" @click="batchDelete()">批量删除</el-button>
+    <div>&#12288;</div>
     <el-tree
       :data="menus"
       :props="defaultProps"
@@ -7,6 +17,10 @@
       show-checkbox
       node-key="catId"
       :default-expanded-keys="expandedKey"
+      :draggable="draggable"
+      :allow-drop="allowDrop"
+      @node-drop="handleDrop"
+      ref="menuTree"
     >
       <span class="custom-tree-node" slot-scope="{ node, data }">
         <span>{{ node.label }}</span>
@@ -76,6 +90,10 @@ export default {
   props: {},
   data() {
     return {
+      pCid: [],
+      draggable: false,
+      updateNodes: [],
+      draggingNodeMaxLevel: 0,
       title: "",
       dialogType: "",
       menus: [],
@@ -104,6 +122,185 @@ export default {
   watch: {},
   // 方法集合
   methods: {
+    // 批量删除
+    batchDelete() {
+      // 1. 获取所有被选中的节点
+      let checkedNodes = this.$refs.menuTree.getCheckedNodes();
+
+      // 2. 将选中的节点的 catId 取出
+      let checkedNodeKeys = [];
+      let checkedNodeNames = [];
+      for (let i = 0; i < checkedNodes.length; i++) {
+        checkedNodeKeys.push(checkedNodes[i].catId);
+        checkedNodeNames.push(checkedNodes[i].name);
+      }
+
+      // 3. 发送请求进行删除
+      this.$confirm(`确定删除【${checkedNodeNames}】菜单项?`, "提示", {
+        confirmButtonText: "确定",
+        cancelButtonText: "取消",
+        type: "warning",
+      })
+        .then(() => {
+          this.$http({
+            url: this.$http.adornUrl("/product/category/delete"),
+            method: "post",
+            data: this.$http.adornData(checkedNodeKeys, false),
+          }).then(({ data }) => {
+            if (data && data.code === 0) {
+              this.$message({
+                message: "操作成功",
+                type: "success",
+                duration: 1500,
+                onClose: () => {
+                  // 1. 获取新的分类数据
+                  this.getTreeMenuData();
+                },
+              });
+            } else {
+              this.$message.error(data.msg);
+            }
+          });
+        })
+        .catch(() => {});
+    },
+
+    // 批量拖拽
+    batchDrag() {
+      this.$http({
+        url: this.$http.adornUrl("/product/category/batch/update"),
+        method: "post",
+        data: this.$http.adornData(this.updateNodes, false),
+      }).then(({ data }) => {
+        if (data && data.code === 0) {
+          this.$message({
+            message: "操作成功",
+            type: "success",
+            duration: 1500,
+            onClose: () => {
+              // 1. 获取新的分类数据
+              this.getTreeMenuData();
+              // 2. 默认展开之前删除分类的父分类
+              this.expandedKey = this.pCid;
+              // 4. 清空拖拽形成的数据
+              this.draggingNodeMaxLevel = 0;
+              this.updateNodes = [];
+              this.pCid = [];
+            },
+          });
+        } else {
+          this.$message.error(data.msg);
+        }
+      });
+    },
+
+    // 拖拽成功完成时触发的事件
+    handleDrop(draggingNode, dropNode, dropType, ev) {
+      console.log(draggingNode, dropNode, dropType);
+      // 1. 获取当前节点拖动后的父节点id 和 拖动后的兄弟节点数组
+      let pCid = 0;
+      let slibings = [];
+      if (dropType == "inner") {
+        pCid = dropNode.data.catId;
+        slibings = dropNode.childNodes;
+      } else {
+        pCid =
+          dropNode.parent.data.catId == undefined
+            ? 0
+            : dropNode.parent.data.catId; // 如果是将当前节点拖动到根节点位置，那么就没有父节点 id，需要置为 0
+        slibings = dropNode.parent.childNodes;
+      }
+      this.pCid.push(pCid);
+
+      // 2. 获取当前节点拖动后的顺序和层级（即在拖动后的父节点的 childNodes 中的序号）
+      for (let i = 0; i < slibings.length; i++) {
+        let updateNode = {};
+        if (slibings[i].data.catId == draggingNode.data.catId) {
+          // 如果是遍历到当前节点，则需要通过的当前节点的 catId 来更新 parentCid、catLevel 和 sort 字段
+          let catLevel = draggingNode.level; // 拖动前的层级
+          if (slibings[i].level != draggingNode.level) {
+            console.log("拖动的节点的层级发生了变化...");
+            // 如果当前节点的层级在拖动前后发生了变化，则需要更新当前节点的层级
+            catLevel = slibings[i].level;
+            // 以及更新当前节点的子节点层级
+            this.updateChildNodesLevel(slibings[i]);
+          }
+          updateNode = {
+            catId: slibings[i].data.catId,
+            parentCid: pCid,
+            catLevel: catLevel,
+            sort: i,
+          };
+        } else {
+          // 如果是遍历到当前节点拖动后的兄弟节点，则需要通过的兄弟节点的 catId 来更新 sort 字段
+          updateNode = { catId: slibings[i].data.catId, sort: i };
+        }
+        this.updateNodes.push(updateNode);
+      }
+
+      console.log("所有要修改的节点", this.updateNodes);
+    },
+
+    // 更新 node 节点的子节点的层级
+    updateChildNodesLevel(node) {
+      if (node.childNodes != null && node.childNodes.length != 0) {
+        for (let i = 0; i < node.childNodes.length; i++) {
+          let childNode = node.childNodes[i].data;
+          // 需要也将这些节点加入到 updateNodes 中更新到数据库
+          this.updateNodes.push({
+            catId: childNode.catId,
+            catLevel: node.childNodes[i].level,
+          });
+          this.updateChildNodesLevel(node.childNodes[i]);
+        }
+      }
+    },
+
+    // 拖拽时判定目标节点能否被放置
+    allowDrop(draggingNode, dropNode, type) {
+      // 核心判断条件：被拖动的当前节点的总层数 + 该节点拖动后所处位置的父节点的层数 <= 3 才可以允许拖动
+      // console.log(draggingNode, dropNode, type);
+
+      // 1. 计算当前拖动节点的子节点的最大层级是多少
+      this.draggingNodeMaxLevel = 0;
+      this.countDraggingNodeTotalLevel(draggingNode);
+
+      // 2. 计算当前拖动节点及其子节点的层数是多少（即当前节点为根节点形成的树有多少层）
+      // 如果 draggingNodeMaxLevel = 0，表示没有子节点，那么其本身层级数即为最大层级数
+      this.draggingNodeMaxLevel =
+        this.draggingNodeMaxLevel == 0
+          ? draggingNode.level
+          : this.draggingNodeMaxLevel;
+      console.log("当前节点的最大层级数", this.draggingNodeMaxLevel);
+      console.log("当前节点本身的层级数", draggingNode.level);
+      let deep = Math.abs(this.draggingNodeMaxLevel - draggingNode.level) + 1;
+      console.log("以当前节点为根节点形成的树有多少层", deep);
+
+      // 3. 计算
+      if (type == "inner") {
+        // 如果是 inner，则表示 dropNode 就是拖动后的父节点
+        return deep + dropNode.level <= 3;
+      } else {
+        // 如果是 next 或者是 prev 则表示 dropNode.parent 才是拖动后的父节点
+        return deep + dropNode.parent.level <= 3;
+      }
+    },
+
+    // 计算当前拖动节点的子节点的最大层级是多少
+    countDraggingNodeTotalLevel(node) {
+      // 使用 node.childNodes 中每一个节点的 level 值去判断，这样能够保证得到的是最新的层级
+      if (node.childNodes != null && node.childNodes.length != 0) {
+        for (let i = 0; i < node.childNodes.length; i++) {
+          this.draggingNodeMaxLevel =
+            node.childNodes[i].level > this.draggingNodeMaxLevel
+              ? node.childNodes[i].level
+              : this.draggingNodeMaxLevel;
+          this.countDraggingNodeTotalLevel(node.childNodes[i]);
+        }
+      }
+    },
+
+    // 重置 category 对象
     resetCategory(
       parentCid,
       catLevel,
